04fffd41704c04f9f67f68ea4419feaeab0ea2c3
20230035 <20230035@students.htl-perg.ac.at>
2025-11-28 16:29:22 +0100

    parameter zu waveLegs und circleJerk hinzugefügt

--- include/algorithm.h
+++ include/algorithm.h
@@ -3,11 +3,12 @@
 
 #include <Kinematics.h>
 #include "calibrations.h"
+#include "servocontrols.h"
 
+extern float defaultWaveLegOffsets[NUM_LEGS];
 
-
-void circleJerk();
-void waveLegs();
+void circleJerk(float speed = 2.5f); // default speed = 2.5f
+void waveLegs(float legOffsets[] = defaultWaveLegOffsets, float speed=0.15f); // default speed = 0.15f
 void initLegs();
 void walkForward(uint16_t steps);
 
--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -2,7 +2,7 @@
 #include "calibrations.h"
 #include "servocontrols.h"
 
-
+float defaultWaveLegOffsets[NUM_LEGS] = {0, 57.6, 115.2, 172.8, 230.4, 288.0};  
 
 static inline int mapToUs(int deg, int inMin, int inMax, int outMin, int outMax) {
     if (deg < inMin) deg = inMin;
@@ -45,9 +45,7 @@ void walkForward(uint16_t steps) {
     }
 }
 
-static float legOffsets[NUM_LEGS] = {0, 57.6, 115.2, 172.8, 230.4, 288.0};  
-
-void waveLegs() {
+void waveLegs(float legOffsets[], float speed) {
     static float t = 0.0f;  // time or phase
     float mid = (FEMUR_MIN + FEMUR_MAX) / 2.0f;
     float amplitude = (FEMUR_MAX - FEMUR_MIN) / 2.0f;
@@ -55,35 +53,35 @@ void waveLegs() {
     for (uint16_t i = 0; i < NUM_LEGS; i++) {
         // Convert offset to radians for sine
         float phase = legOffsets[i] * (PI / 180.0f);
-        float servoAngle = mid + amplitude * sin(t + phase);
+        femur[i].angle = mid + amplitude * sin(t + phase);
 
-        moveServo(i, 1, servoAngle);
+        moveServo(i, 1, femur[i].angle);
     }
 
-    t += 0.1f;  // increment time, controls speed
+    t += speed;  // increment time, controls speed
     delay(30);  // adjust delay as needed
 }
 
-void circleJerk() {
+void circleJerk(float speed) {
     for (int i = 0; i < NUM_LEGS; i++) {
         moveServo(i, 0, coxa[i].angle);
-        coxa[i].angle += coxa[i].dir * 2;
+        coxa[i].angle += coxa[i].dir * speed;
         if (coxa[i].angle <= COXA_MIN || coxa[i].angle >= COXA_MAX) {
             coxa[i].dir = -coxa[i].dir;
         }
 
         moveServo(i, 1, femur[i].angle);
-        femur[i].angle += femur[i].dir * 2;
+        femur[i].angle += femur[i].dir * speed;
         if (femur[i].angle <= FEMUR_MIN || femur[i].angle >= FEMUR_MAX) {
             femur[i].dir = -femur[i].dir;
         }
 
         moveServo(i, 2, tibia[i].angle);
-        tibia[i].angle += tibia[i].dir * 2;
+        tibia[i].angle += tibia[i].dir * speed;
         if (tibia[i].angle <= TIBIA_MIN || tibia[i].angle >= TIBIA_MAX) {
             tibia[i].dir = -tibia[i].dir;
         }
 
     }
-    delay(10);
+    delay(7);
 }
